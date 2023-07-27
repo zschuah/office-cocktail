093494b4dee14d6bacd3c8d01d419db6ba435cd9
zschuah <zschuah@hotmail.com>
2023-07-27 13:12:36 +0800

    final version 2023

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-primary fixed bottom-0 left-0 right-0">
+    <footer className="bg-primary fixed bottom-0 left-0 right-0 z-20">
       <div className="max-w-5xl mx-auto pb-1 pr-4">
         <small className="block text-right text-primary-content">
           Copyright &copy; 2023 zschuah. All Rights Reserved.
